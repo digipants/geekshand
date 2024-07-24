3e91af2c143fb778d09edcf04d80df259396710b
Bhupendra Kumar <bhupendra@digipants.com>
2024-07-24 14:13:48 +0530

    folders

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/669bc9a3cac1844b1f7fcb51');
+    eleventyConfig.addPassthroughCopy('./src/64184874f371bc785f2236eb');
     eleventyConfig.addPassthroughCopy('./src/robots.txt');
     eleventyConfig.addPassthroughCopy('./src/sitemap.xml');
 
